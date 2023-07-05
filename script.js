f6eb0a7ef6d96d2d2d08ff17d8e5952726854745
Barasa-Micah <barasamica@gmail.com>
2023-07-05 14:15:56 +0300

    Added a log in button

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Variables for task to loop at the home page
 
 var typed = new Typed (".input",{
-    strings:['Update created task','Track task progress', 'Help you prioritize tasks','Help you collaborate effecitively'],
+    strings:['Create task','Track task progress', 'Prioritize tasks','Collaborate effectively'],
     typSpeed:200,
     backSpeed:200,
     loop:true
