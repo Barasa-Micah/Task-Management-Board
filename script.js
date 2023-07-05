d8b7ddffb90acd4a15e22580447f694f4af9ec69
Barasa-Micah <barasamica@gmail.com>
2023-07-06 02:28:13 +0300

    Added a functionality in the login page

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ var typed = new Typed (".input",{
     loop:true
 })
     
+// const wrapper = document.querySelector('.wrapper');
+// const loginLink = document.querySelector('.login-link');
+// const registerLink = document.querySelector('.register-link');
+// const btnPopup = document.querySelector('.register-link');
+
+
+// registerLink.addEventListener('click', () =>{
+//     wrapper.classList.add('active');
+// });
+
+// loginLink.addEventListener('click', () => {
+//     wrapper.classListlist.remove('active')
+// })
+
+// btnPopup.addEventListener('cick', ()=>{
+//     wrapper.classList.add('active-popup')
+// })
+
