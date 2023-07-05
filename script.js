e235105637ded794a0f68d86210993b57ed0f43f
Barasa-Micah <barasamica@gmail.com>
2023-07-05 14:03:10 +0300

    Updated the colors on the styles section

---
+++ script.js
@@ -0,0 +1,9 @@
+//Variables for task to loop at the home page
+
+var typed = new Typed (".input",{
+    strings:['Update created task','Track task progress', 'Help you prioritize tasks','Help you collaborate effecitively'],
+    typSpeed:200,
+    backSpeed:200,
+    loop:true
+})
+    
